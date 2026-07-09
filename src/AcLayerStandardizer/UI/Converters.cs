3e9ecf8ffe0800c8f5e6a2701079eb5994f40c44
yiannias <chris@yiannias.com>
2026-07-08 23:23:23 -0500

    feat: add floating match-type filter toggles with opacity, visibility, and target node coloring

--- src/AcLayerStandardizer/UI/Converters.cs
+++ src/AcLayerStandardizer/UI/Converters.cs
@@ -5,6 +5,15 @@ using System.Windows.Media;
 
 namespace AcLayerStandardizer.UI;
 
+public class BooleanToOpacityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is true ? 1.0 : 0.5;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
+
 public class BooleanToVisibilityConverter : IValueConverter
 {
     public static readonly BooleanToVisibilityConverter Instance = new();
